f5c38682a995e93dc297852562f538561e62d034
AashishSec <65489287+aashishsec@users.noreply.github.com>
2023-11-23 12:48:15 +0530

    Add files via upload

---
+++ pyDirBruteMark-1.py
@@ -0,0 +1,187 @@
+import requests,colorama,random,argparse,concurrent.futures
+
+from colorama import Fore, Back, Style
+
+colorama.init(autoreset=True)
+
+green = Fore.GREEN
+
+magenta = Fore.MAGENTA
+
+cyan = Fore.CYAN
+
+mixed = Fore.RED + Fore.BLUE
+
+red = Fore.RED
+
+blue = Fore.BLUE
+
+yellow = Fore.YELLOW
+
+white = Fore.WHITE
+
+reset = Style.RESET_ALL
+
+colors = [magenta,cyan,mixed,red,blue,yellow, white]
+
+random_color = random.choice(colors)
+
+bold = Style.BRIGHT
+
+def banner():
+
+    print(f'''{bold}{random_color}
+
+██████╗░██╗██████╗░░░░██████╗░██████╗░██╗░░░██╗████████╗███████╗
+██╔══██╗██║██╔══██╗░░░██╔══██╗██╔══██╗██║░░░██║╚══██╔══╝██╔════╝
+██║░░██║██║██████╔╝░░░██████╦╝██████╔╝██║░░░██║░░░██║░░░█████╗░░
+██║░░██║██║██╔══██╗░░░██╔══██╗██╔══██╗██║░░░██║░░░██║░░░██╔══╝░░
+██████╔╝██║██║░░██║██╗██████╦╝██║░░██║╚██████╔╝░░░██║░░░███████╗
+╚═════╝░╚═╝╚═╝░░╚═╝╚═╝╚═════╝░╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░╚══════╝
+      
+        Author   : Aashish💕💕  
+                                              
+        Github   : https://github.com/aashish36
+          
+        DirBrute is a tool designed to efficiently probe for alive endpoints from a provided wordlist list.
+          
+      ''')
+
+
+parser=argparse.ArgumentParser(description=f"{bold}{random_color} DirBrute is a tool designed to efficiently probe for alive endpoints from a provided wordlist list.")
+
+parser.add_argument('-d','--Domain',metavar='list',type=str,required=True,help=f"[{bold}{random_color}INFO]: {bold}{random_color}domain.")
+
+parser.add_argument('-w','--wordlist',metavar='list',type=str,required=True,help=f"[{bold}{random_color}INFO]: {bold}{random_color}List of wordlist.")
+
+parser.add_argument('-o','--output',metavar='output',type=str,default="DirBrute_output.txt",required=False,help=f"[{bold}{random_color}INFO]: {bold}{random_color}File to save our output.")
+
+parser.add_argument("-c", "--concurrency", help=f"[{bold}{random_color}INFO{random_color}]: {bold}{random_color}Concurrency level to make fast process", type=int, default=10)
+
+parser.add_argument("-t", "--threads", help=f"[{bold}INFO{random_color}]: {random_color}{random_color}Threading level to make fast process", type=int, default=4)
+
+args=parser.parse_args()
+
+Domain=args.Domain
+
+output=args.output
+
+concurrency=args.concurrency
+
+threads=args.threads
+
+wordlist=args.wordlist
+
+global_output=[]
+
+
+def DirBrute(domain,path):
+
+    global global_output
+
+    try:
+
+            if domain[0:5]=="https" or domain[0:7]=="http://":
+                  
+                  url=f"{domain}/{path}"
+
+            else:
+                  
+                  url="https://{}/{}".format(domain,path)
+
+            request=requests.get(url,timeout=10)
+
+            statusCode=request.status_code
+
+            content_length = request.headers.get('Content-Length')
+
+            if statusCode== 200:
+
+               if content_length is not None:
+                   
+                   print(f"{green}(Status: {statusCode}) --[Size: {content_length}]---> {url}")
+
+                   global_output.append(f"(Status: {statusCode}) --[Size: {content_length}]---> {url}\n")
+
+               else:
+                   
+                   print(f"{green}(Status: {statusCode}) --[Size: {len(request.content)}]---> {url}")
+
+                   global_output.append(f"(Status: {statusCode}) --[Size: {len(request.content)}]---> {url}\n")
+
+            else:
+               
+                print(f"{random_color}(Status: {statusCode}) --[Size: {len(request.content)}]---> {url}")
+
+                global_output.append(f"(Status: {statusCode}) --[Size: {len(request.content)}]---> {[path]}\n")
+                
+    except KeyboardInterrupt as e:
+        
+            print(f"[{blue}INFO{random_color}]: httpAlive says BYE!")
+        
+            exit()
+                
+    except Exception as e:
+        
+           pass
+
+def saveOutput(output):
+
+    with open(output, 'w') as file:
+        
+        file.writelines(global_output)
+
+
+def threading(Domain,paths):
+    
+    try:
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=concurrency*threads) as executor:
+            
+           futures = [executor.submit(DirBrute, Domain,path) for path in paths]
+           
+        concurrent.futures.wait(futures)
+           
+    except KeyboardInterrupt as e:
+        
+        print(f"[{bold}INFO{random_color}]: dirBrute says exit!")
+        
+        exit()
+
+    except Exception as e:
+        
+        pass 
+
+
+def main(wordlist):
+        
+    banner()
+
+    path=[]
+    
+    try:
+        with open(wordlist,"r") as p:
+       
+           paths=p.read().splitlines()
+        
+           for p in paths:
+          
+               path.append(p)
+
+        threading(Domain,path)
+
+        saveOutput(output) 
+           
+    except KeyboardInterrupt as e:
+        
+        print(f"[{blue}INFO{random_color}]: dirbrute says exit!")
+        
+        exit()
+
+    except Exception as e:
+        
+        pass 
+
+if __name__ == "__main__":
+
+    main(wordlist)
