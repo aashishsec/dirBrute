5daa2e79cc744abdce2c5f8f79862d8ddebdb0b3
AashishSec ఆశిష్ <65489287+aashishsec@users.noreply.github.com>
2024-01-28 12:22:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='DirBrute',
+    version='1.0.2',
+    description='DirBrute is a tool designed to efficiently probe for alive endpoints from a provided wordlist list.',
+    author='Bande Aashish',
+    url='https://github.com/aashishsec/dirBrute',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'httpx',
+        'colorama',
+    ],
+    extras_require={
+        'dev': ['argparse', 'concurrent.futures'],
+    },
+    entry_points={
+        'console_scripts': [
+            'dirbrute=dirbrute.dirbrute:main',
+        ],
+    },
+)
 
